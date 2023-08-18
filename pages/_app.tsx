ba16c49dcdba95e4ff20e14b3252ff0ac2f265ff
Brillaugte <58943684+Brillaugte@users.noreply.github.com>
2023-08-18 14:08:57 +0300

    vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import Layout from './components/layout';
 import { Mumbai, Arbitrum } from "@thirdweb-dev/chains";
 import { Web3Button } from '@thirdweb-dev/react';
 import {useState} from 'react';
-
+import DepositComponent from './components/deposit';
 
 
 
@@ -27,38 +27,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
         </Layout>
         <div>
-        <label>
-        Enter the desired amount:
-        <input
-          type="number"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-      </label>
-      <Web3Button
-        contractAddress="0x300f5ECE2c9D4265C9DA9021582ed1B71F403549"
-        action={(contract) => {
-          contract.call('approve', ["0x5275396224FCbCb9Eb1217fc6Fae4B3DDe05A1a2", amount]);
-        }}
-      >
-        approve
-      </Web3Button>
-      <Web3Button
-        contractAddress="0x5275396224FCbCb9Eb1217fc6Fae4B3DDe05A1a2"
-        action={(contract) => {
-          contract.call('deposit', [amount]);
-        }}
-      >
-        deposit
-      </Web3Button>
-      <Web3Button
-        contractAddress="0x5275396224FCbCb9Eb1217fc6Fae4B3DDe05A1a2"
-        action={(contract) => {
-          contract.call('withdraw', [amount]);
-        }}
-      >
-        withdraw
-      </Web3Button>
+        <DepositComponent />
     </div>
       
     </ThirdwebProvider>
