201a4d7b8bc49e7a5e54176f22938aacaaeba5b1
Brillaugte <58943684+Brillaugte@users.noreply.github.com>
2023-08-18 12:58:15 +0300

    vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import { Mumbai, Arbitrum } from "@thirdweb-dev/chains";
 import { Web3Button } from '@thirdweb-dev/react';
 import {useState} from 'react';
 
+import { ethers } from "ethers";
+
 
 
 const address = "0xd0dDF915693f13Cf9B3b69dFF44eE77C901882f8";
@@ -16,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       activeChain={Mumbai}
-      //signer={new ethers.providers.Web3Provider(window.ethereum).getSigner()}
+      signer={new ethers.providers.Web3Provider(window.ethereum).getSigner()}
       clientId="3872f830028481ed73e2103b84ae94b7"
     >
       <Layout>
