bb27111e83a0075131a34d90864e8e17c7d2c0e5
Brillaugte <58943684+Brillaugte@users.noreply.github.com>
2023-08-18 11:53:22 +0300

    vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,14 @@ import { Mumbai, Arbitrum } from "@thirdweb-dev/chains";
 import WalletComponent from "./components/depositWithdraw";
 
 
-const activeChain = "Mumbai";
 const address = "0xd0dDF915693f13Cf9B3b69dFF44eE77C901882f8";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
+      activeChain={Mumbai}
+      //signer={new ethers.providers.Web3Provider(window.ethereum).getSigner()}
+      clientId="3872f830028481ed73e2103b84ae94b7"
     >
       <Component {...pageProps} />
       {address && <WalletComponent account={address} spender={address} />} 
