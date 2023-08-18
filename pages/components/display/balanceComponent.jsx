0a5365b38639acc55af474c6f48636a2f0d033b3
Brillaugte <58943684+Brillaugte@users.noreply.github.com>
2023-08-18 15:32:23 +0300

    vercel

--- pages/components/display/balanceComponent.jsx
+++ pages/components/display/balanceComponent.jsx
@@ -6,18 +6,19 @@ export default function BalanceComponent() {
   const { contract: contract1 } = useContract("0x300f5ECE2c9D4265C9DA9021582ed1B71F403549");
   const { contract: contract2 } = useContract("0x5275396224FCbCb9Eb1217fc6Fae4B3DDe05A1a2");
 
-  const balance1 = useContractRead(contract1, "balanceOf", [account]);
-  const balance2 = useContractRead(contract2, "b", [account]);
+  const [reloadKey, setReloadKey] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      useContractRead(contract1, "balanceOf", [account]);
-      useContractRead(contract2, "b", [account]);
+      setReloadKey((prevKey) => prevKey + 1);
     }, 30000); // 30 seconds
 
     // Clear the interval when the component unmounts
     return () => clearInterval(intervalId);
-  }, [contract1, contract2, account]);
+  }, []);
+
+  const balance1 = useContractRead(contract1, "balanceOf", [account, reloadKey]);
+  const balance2 = useContractRead(contract2, "b", [account, reloadKey]);
 
   return (
     <div>
