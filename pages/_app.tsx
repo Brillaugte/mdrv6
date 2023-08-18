0d2e42f9052a9122e6906b130c0869afa8009879
Brillaugte <58943684+Brillaugte@users.noreply.github.com>
2023-08-18 13:02:32 +0300

    vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,20 +11,18 @@ import { ethers } from "ethers";
 
 
 const address = "0xd0dDF915693f13Cf9B3b69dFF44eE77C901882f8";
+const MDRV = "0x5275396224FCbCb9Eb1217fc6Fae4B3DDe05A1a2"; // MDRV contract
+//const events = await contract.events.getEvents("Quote");
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [amount, setAmount] = useState('');
 
   return (
-    <ThirdwebProvider
-      activeChain={Mumbai}
-      signer={new ethers.providers.Web3Provider(window.ethereum).getSigner()}
-      clientId="3872f830028481ed73e2103b84ae94b7"
-    >
+<div>
       <Layout>
         <Component {...pageProps} />
         </Layout>
-        <div>
+        
         <label>
         Enter the desired amount:
         <input
@@ -59,8 +57,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         withdraw
       </Web3Button>
     </div>
-      
-    </ThirdwebProvider>
 
   );
 }
