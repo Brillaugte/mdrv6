f47bccfa1a14614466542b6c0dbb6c4df9062f35
Brillaugte <58943684+Brillaugte@users.noreply.github.com>
2023-08-18 15:27:09 +0300

    vercel

--- pages/components/display/balanceComponent.jsx
+++ pages/components/display/balanceComponent.jsx
@@ -1,27 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useContract, useContractRead } from "@thirdweb-dev/react";
 
-const contractAddress1 = "0x300f5ECE2c9D4265C9DA9021582ed1B71F403549";
-const contractAddress2 = "0x5275396224FCbCb9Eb1217fc6Fae4B3DDe05A1a2";
-
 export default function BalanceComponent() {
   const [account, setAccount] = useState("0xd0dDF915693f13Cf9B3b69dFF44eE77C901882f8"); // Default address
-  const { contract: contract1 } = useContract(contractAddress1);
-  const { contract: contract2 } = useContract(contractAddress2);
+  const { contract: contract1 } = useContract("0x300f5ECE2c9D4265C9DA9021582ed1B71F403549");
+  const { contract: contract2 } = useContract("0x5275396224FCbCb9Eb1217fc6Fae4B3DDe05A1a2");
 
-  const [balance1, setBalance1] = useState(0);
-  const [balance2, setBalance2] = useState(0);
+  const balance1 = useContractRead(contract1, "balanceOf", [account]);
+  const balance2 = useContractRead(contract2, "b", [account]);
 
   useEffect(() => {
-    const fetchBalances = async () => {
-      const { data: balanceData1 } = useContractRead(contract1, "balanceOf", [account]);
-      const { data: balanceData2 } = useContractRead(contract2, "balanceOf", [account]);
-      setBalance1(balanceData1);
-      setBalance2(balanceData2);
-    };
-
     const intervalId = setInterval(() => {
-      fetchBalances();
+      useContractRead(contract1, "balanceOf", [account]);
+      useContractRead(contract2, "b", [account]);
     }, 30000); // 30 seconds
 
     // Clear the interval when the component unmounts
@@ -38,8 +29,8 @@ export default function BalanceComponent() {
           onChange={(e) => setAccount(e.target.value)}
         />
       </div>
-      <div>Balance 1: {balance1}</div>
-      <div>Balance 2: {balance2}</div>
+      <div>Balance 1: {balance1.data}</div>
+      <div>Balance 2: {balance2.data}</div>
     </div>
   );
 }
